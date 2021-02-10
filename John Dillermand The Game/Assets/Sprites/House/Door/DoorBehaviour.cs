aee4cb87c4933a94acd51e51f96e6344bc045f02
Christian <ChristiaNotarmaso@gmail.com>
2021-02-10 23:38:41 +0100

    Hus og hitboxes!

---
+++ John Dillermand The Game/Assets/Sprites/House/Door/DoorBehaviour.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour
+{
+    public Sprite doorClosed;
+    public Sprite doorOpened;
+    SpriteRenderer sr;
+    public bool isOnCollider = false;
+
+    private bool isOpened = false;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            if (Input.GetKeyDown("e") && isOnCollider)
+            {
+                if (!isOpened)
+                {
+                    GetComponent<BoxCollider2D>().isTrigger = true;
+                    setDoorOpen();              
+                    isOpened = true;
+                }
+                else
+                {
+                    GetComponent<BoxCollider2D>().isTrigger = false;
+                    setDoorClosed();                
+                    isOpened = false;
+                }
+            }
+
+        
+    }
+
+    void setDoorOpen() {
+        sr.sprite = doorOpened;
+    }
+    public void setLayer(int number) {
+        sr.sortingOrder = number;
+    }
+    public void setDoorClosed()
+    {
+        sr.sprite = doorClosed;
+        GetComponent<BoxCollider2D>().isTrigger = false;
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        isOpened = false;
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        isOnCollider = true;
+          
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        isOnCollider = false;
+
+    }
+
+}
---
+++ John Dillermand The Game/Assets/Sprites/House/Door/TriggersDoor.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggersDoor : MonoBehaviour
+{
+    GameObject door, player, roof;
+    GameObject [] wall;
+    DoorBehaviour db;
+    // Start is called before the first frame update
+    void Start()
+    {
+        door = GameObject.Find("Door");
+        player = GameObject.Find("Player");
+        wall = GameObject.FindGameObjectsWithTag("wall");
+        db = door.GetComponent<DoorBehaviour>();
+        roof = GameObject.FindGameObjectWithTag("roof");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (this.name == "HBDoorFront")
+        {
+            //db.setLayer(1);
+            db.setDoorClosed();
+            player.GetComponent<SpriteRenderer>().sortingOrder = 2;
+
+            db.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+            roof.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+            foreach (GameObject obWall in wall)
+            {
+                obWall.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+        
+            }
+        }
+        else if (this.name == "HBDoorBack") {
+
+            player.GetComponent<SpriteRenderer>().sortingOrder = 0;
+            db.setDoorClosed();
+            db.isOnCollider = false;
+            db.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .7f);
+            roof.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0f);
+            foreach (GameObject obWall in wall) {
+            
+               
+                obWall.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .7f);
+            }
+            
+        } 
+    }
+}
